305cc238714b8e415d43b3e8b636d673b53e3239
Siddhant Lamba <87963421+georgiansid@users.noreply.github.com>
2022-11-04 11:48:15 +0530

    Add files via upload

---
+++ bubblesort.js
@@ -0,0 +1,26 @@
+"use strict";
+class sortAlgorithms {
+    constructor(time) {
+        this.list = document.querySelectorAll(".cell");
+        this.size = this.list.length;
+        this.time = time;
+        this.help = new Helper(this.time, this.list);
+    }
+
+    // BUBBLE SORT
+    BubbleSort = async () => {
+        for(let i = 0 ; i < this.size - 1 ; ++i) {
+            for(let j = 0 ; j < this.size - i - 1 ; ++j) {
+                await this.help.mark(j);
+                await this.help.mark(j+1);
+                if(await this.help.compare(j, j+1)) {
+                    await this.help.swap(j, j+1);
+                }
+                await this.help.unmark(j);
+                await this.help.unmark(j+1);
+            }
+            this.list[this.size - i - 1].setAttribute("class", "cell done");
+        }
+        this.list[0].setAttribute("class", "cell done");
+    }
+ };
